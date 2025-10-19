d9c17ba0a3521dd4feab6ad73402c500fa4af411
nikkibubencik <bubnikki4@gmail.com>
2025-10-19 17:07:11 -0400

    add faccumulated total for transactionList

--- src/app/transactions/page.tsx
+++ src/app/transactions/page.tsx
@@ -4,6 +4,7 @@ import { Transaction } from "@/utils/transactionStorage";
 import styles from './TransactionList.module.css';
 import { useTransactions } from "@/hooks/useTransactions";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
 interface Props {
   transactions: Transaction[];
@@ -11,6 +12,12 @@ interface Props {
 
 const TransactionList: React.FC<Props> =() =>{
     const { transactions, deleteTransaction } = useTransactions();
+    const [accumulatedTotal, setAccumulatedTotal] = useState<number>(0);
+
+    useEffect(() => {
+        const total = transactions.reduce((acc, transaction) => transaction.transactionType == 'income' ? acc + transaction.amount : acc - transaction.amount, 0);
+        setAccumulatedTotal(total);
+    }, [transactions]);
 
     const eraseTransaction = (id: number, name: string, date: string) =>{
         if(window.confirm("You are about to delete " + name + " from " + date)){
@@ -23,6 +30,13 @@ const TransactionList: React.FC<Props> =() =>{
             {transactions.length > 0 ?
             <>
                 <h1>Your Transactions</h1>
+                <p>
+                    <span style={{fontWeight: 600}}>Total: </span> 
+                    {accumulatedTotal.toLocaleString('en-US', {
+                        style: 'currency',
+                        currency: 'USD',
+                    })}
+                </p>
                 {transactions.map((transaction, index) => 
                     <TransactionItem transaction={transaction} eraseTransaction={eraseTransaction} key={transaction.id}/>
                 ) }
