77d2579b09e1cdb27091522f690b6fefb392ba44
nikkibubencik <bubnikki4@gmail.com>
2025-10-24 21:45:47 -0400

    add styling to budget list display

--- src/app/budgets/page.tsx
+++ src/app/budgets/page.tsx
@@ -3,19 +3,34 @@ import { Budget } from "@/utils/budgetStorage";
 import BudgetItem from "@/components/Budget/BudgetItem/BudgetItem";
 import styles from './BudgetList.module.css';
 import { useBudget } from "@/hooks/useBudget";
+import Link from "next/link";
 
 interface Props {
   budgets: Budget[];
 }
 
 const BudgetList: React.FC<Props> =() =>{
-    const {budgets} = useBudget();
+    const {budgets, deleteBudget} = useBudget();
+
+    const eraseBudget = (id: number, name: string, category: string) =>{
+        if(window.confirm("You are about to delete " + name + "(" + category + ")")){
+            deleteBudget(id);
+        }
+    }
+
     return (
-        <div className={styles.budgetContainer}>
+        <div className={`componentGroup styles.budgetContainer`}>
             <h2>Your Budgets</h2>
-            {budgets.map((budget, index) => 
-                <BudgetItem budget={budget} key={index}/>
-            )}
+            { budgets.length > 0 ?
+                budgets.map((budget, index) => 
+                    <BudgetItem budget={budget} eraseBudget={eraseBudget} key={index}/>
+                )
+                :
+                <>
+                    <p>No budgets to display</p>
+                    <Link href="/budget-form">Add a budget</Link>
+                </>
+            }
         </div>
     );
 }
--- src/components/Budget/BudgetItem/BudgetItem.tsx
+++ src/components/Budget/BudgetItem/BudgetItem.tsx
@@ -1,40 +1,60 @@
 import { useState } from "react";
-import { Budget } from "../../../utils/budgetStorage";
+import { Budget } from "@/utils/budgetStorage";
 import styles from './BudgetItem.module.css';
+import { useRouter } from "next/navigation";
 
 interface Props {
   budget: Budget;
+  eraseBudget: (id:number, name: string, category: string) => void;
 }
 
 
-const BudgetItem: React.FC<Props> = ({ budget }) =>{
-    const { name, description, limit, expenseCategory, endDate} = budget;
+const BudgetItem: React.FC<Props> = ({ budget, eraseBudget }) =>{
+    const { id, name, description, limit, expenseCategory, endDate} = budget;
     const [seeDesc, setSeeDesc] = useState<boolean>(false);
+    const router = useRouter();
 
     const toggleSeeDesc = () => {
         setSeeDesc(!seeDesc);
     }
 
+    const editBudget = () => {
+        router.push(`/budget-form/${id}`);
+    }
+
     return (
-        <div className={styles.budgetCard}>
-            <div className={styles.budgetInfo}>
-                
+        <div className="itemCard">
+            <div className="itemInfo">
                 <h3>{name}</h3>
-                <p>{endDate}</p>
-            
-                <p>${limit.toFixed(2)}</p>
-                <p>{expenseCategory}</p>
+                <p className="important">
+                    {limit.toLocaleString('en-US', {
+                        style: 'currency',
+                        currency: 'USD',
+                    })}
+                </p>
             </div>
-            {description &&
-                <>
-                    {seeDesc && <p>{description}</p>}
-                    <button onClick={toggleSeeDesc}>
-                        {seeDesc ? 
-                            'Hide Description' 
-                            : 'See Description'}
-                    </button>
-                </>
-            }
+            <div className="itemInfo">
+                <p>{expenseCategory}</p>
+                <p>{endDate}</p>
+            </div>
+            <div className="itemInfo">
+                <div>
+                    {description &&
+                        <>
+                            {seeDesc && <p>{description}</p>}
+                            <button className="button" onClick={toggleSeeDesc}>
+                                {seeDesc ? 
+                                    '▲ Hide Description' 
+                                    : '▼ See Description'}
+                            </button>
+                        </>
+                    }
+                </div>
+                <div>
+                    <button className="editBtn" onClick={editBudget}></button>
+                    <button className="deleteBtn" onClick={() => eraseBudget(id, name, expenseCategory)}></button>
+                </div>
+            </div>
         </div>
     );
 }
